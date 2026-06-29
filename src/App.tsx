1efd3064fa2fa17b23419ba63bfd1656cf1a6f11
emirleylek <emirleylek@piton.com.tr>
2026-06-29 15:58:30 +0300

    #100-002 feat: Added vercel analytics.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // src/App.tsx
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import LifeSpent from "./LifeSpent";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <>
       <LifeSpent />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
